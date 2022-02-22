04a4cbd4802b177ef7ba332eefca39581d9c600a
Diego Yanez <diego_alex40@hotmail.com>
2022-02-22 08:44:07 -0500

    Ejercicio 1 resuelto

---
+++ js/ejercico 1.js
@@ -0,0 +1,7 @@
+document.getElementById("tipo1").innerHTML = "Ejemplo del tipo 1 de Output";
+
+document.write("Ejemplo de texto con document.write");
+
+alert("Ejemplo de output con Window.alert");
+
+console.log ("Hola Mundo");
